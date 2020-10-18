ab2fb08aab6340d65b909c09f02ba236848e4418
abauruel <abauruel@gmail.com>
2020-10-18 11:04:46 -0300

    feature: add banckend functions

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import route from './routes'
+import './database/connection'
+import path from 'path'
+import 'express-async-errors'
+import errorHandler from './errors/handler'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(route)
+app.use('/uploads', express.static(path.join(__dirname,'..','uploads')))
+app.use(errorHandler)
+app.listen(3333)
